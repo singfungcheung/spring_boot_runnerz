749ac6150a0e6c594efc135c2fdf397e17fc54e0
singfungcheung <singfung.cheung@gmail.com>
2024-03-21 10:44:02 -0400

    Added a Run RECORD and Location ENUM

--- src/main/java/dev/singfung/runnerz/RunnerzApplication.java
+++ src/main/java/dev/singfung/runnerz/RunnerzApplication.java
@@ -1,10 +1,17 @@
 package dev.singfung.runnerz;
 
+import dev.singfung.runnerz.run.Location;
+import dev.singfung.runnerz.run.Run;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
 public class RunnerzApplication {
@@ -14,6 +21,14 @@ public class RunnerzApplication {
 
 		SpringApplication.run(RunnerzApplication.class, args);
 		log.info("Testing!");
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			Run run = new Run(1, "First Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR);
+			log.info("Run: " + run);
+		};
 	}
 
 }
---
+++ src/main/java/dev/singfung/runnerz/run/Location.java
@@ -0,0 +1,5 @@
+package dev.singfung.runnerz.run;
+
+public enum Location {
+    INDOOR, OUTDOOR
+}
