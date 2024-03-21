00c143a584b91780c3b27fb899b2485d1c68af21
singfungcheung <singfung.cheung@gmail.com>
2024-03-20 21:16:28 -0400

    Added a logger

--- src/main/java/dev/singfung/runnerz/RunnerzApplication.java
+++ src/main/java/dev/singfung/runnerz/RunnerzApplication.java
@@ -1,13 +1,19 @@
 package dev.singfung.runnerz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class RunnerzApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(ApplicationContext.class);
 	public static void main(String[] args) {
+
 		SpringApplication.run(RunnerzApplication.class, args);
+		log.info("Testing!");
 	}
 
 }
