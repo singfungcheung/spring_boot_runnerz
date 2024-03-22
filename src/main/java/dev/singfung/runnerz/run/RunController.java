0c18bd7add2c01d8fd1a00b564daceac51b85f96
singfungcheung <singfung.cheung@gmail.com>
2024-03-22 19:04:35 -0400

    Can do CREATE, UPDATE, and DELETE from database

--- src/main/java/dev/singfung/runnerz/run/RunController.java
+++ src/main/java/dev/singfung/runnerz/run/RunController.java
@@ -23,38 +23,38 @@ public class RunController {
     List<Run> findAll() {
         return runRepository.findAll();
     }
-//
-//    @GetMapping("/{id}")
-//    Run findById(@PathVariable Integer id) {
-//        Optional<Run> run = runRepository.findById(id);
-//        if (run.isEmpty()) {
-//            throw new RunNotFoundException();
-//        }
-//        else {
-//            return run.get();
-//        }
-//    }
-//
-//    // Post
-//    @ResponseStatus(HttpStatus.CREATED)
-//    @PostMapping("")
-//    void create(@Valid @RequestBody Run run) {
-//        runRepository.create(run);
-//    }
-//
-//    // Put
-//    @ResponseStatus(HttpStatus.NO_CONTENT)
-//    @PutMapping("/{id}")
-//    void update(@RequestBody Run run, @PathVariable Integer id) {
-//        runRepository.update(run, id);
-//    }
-//
-//    // Delete
-//    @ResponseStatus(HttpStatus.NO_CONTENT)
-//    @DeleteMapping("/{id}")
-//    void delete(@PathVariable Integer id) {
-//        runRepository.delete(id);
-//    }
+
+    @GetMapping("/{id}")
+    Run findById(@PathVariable Integer id) {
+        Optional<Run> run = runRepository.findById(id);
+        if (run.isEmpty()) {
+            throw new RunNotFoundException();
+        }
+        else {
+            return run.get();
+        }
+    }
+
+    // Post
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    void create(@Valid @RequestBody Run run) {
+        runRepository.create(run);
+    }
+
+    // Put
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@Valid @RequestBody Run run, @PathVariable Integer id) {
+        runRepository.update(run, id);
+    }
+
+    // Delete
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id) {
+        runRepository.delete(id);
+    }
 //    private List<Run> runs = new ArrayList<>();
 //    @RequestMapping("/hello")
 //    String home() {
